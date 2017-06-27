131afc29262c4ebc769cf3267a8f8cd8f36721c6
Yakub <jacobshavel@gmail.com>
2017-06-27 14:52:32 +0200

    Added some text, practicing '-am'

--- Recorder/oX/ViewController.swift
+++ Recorder/oX/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    print("I am practicing committing to git, so I added this text")
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
